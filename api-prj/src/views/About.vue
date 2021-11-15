f40212c80e7829878d6880b75f1a78d4287ef96f
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-11-15 11:49:55 -0500

    k

--- api-prj/src/views/About.vue
+++ api-prj/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="input-div">
+  <span>Latitude:</span>
+  <input v-model='LatValue' type="number" class="latitude-value" >
+  <span>Longitude:</span>
+  <input v-model='LongValue' type="number" class="longitude-value">
+  <button v-on:click = 'returnValue' class="submit-button">Submit</button>
+</div>
 </template>
+
+<style>
+input{
+  border-radius: 5px;
+}
+</style>
+
+<script>
+export default {
+  data(){
+return{
+  test1: 'data',
+  LatValue: '',
+  LongValue:'',
+
+}
+  }, 
+  methods: {
+    returnValue(){
+        console.log('clicked');
+
+    },
+  },
+}
+</script>
+
+
